5273d8c83a34047dd978158c1bc5c55997522b75
Refinery <refinery@gopherstack>
2026-06-06 08:54:51 -0500

    WIP: checkpoint (auto)

--- services/rds/backend.go
+++ services/rds/backend.go
@@ -988,7 +988,7 @@ func (b *InMemoryBackend) CreateDBInstance(
 		DBClusterIdentifier:              opts.DBClusterIdentifier,
 		Engine:                           engine,
 		EngineVersion:                    opts.EngineVersion,
-		DBInstanceStatus:                 instanceStatusAvailable,
+		DBInstanceStatus:                 instanceStatusCreating,
 		MasterUsername:                   masterUser,
 		DBName:                           dbName,
 		Endpoint:                         endpoint,
