48f37f79eba561c71dc7009dbf1adca67d9ca5cd
Witness Patrol <blackbird7181@gmail.com>
2026-06-18 04:46:39 -0500

    WIP: checkpoint (auto)

--- services/s3/object_ops.go
+++ services/s3/object_ops.go
@@ -196,7 +196,7 @@ func (h *S3Handler) headObject(
 
 	if errors.Is(err, ErrDeleteMarker) {
 		w.Header().Set("X-Amz-Delete-Marker", "true")
-		w.WriteHeader(http.StatusNotFound)
+		w.WriteHeader(http.StatusMethodNotAllowed)
 
 		return
 	}
